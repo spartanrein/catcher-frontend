133213465a2e342217efaa7ef59ea0c838cdaf82
Rein de Guzman <37677855+spartanrein@users.noreply.github.com>
2024-03-19 13:48:07 +0800

    fix endpoint

--- src/services/scores.js
+++ src/services/scores.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const scoreApi = createApi({
     reducerPath: 'scoresApi',
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API}),
+    baseQuery: fetchBaseQuery({ baseUrl: `${process.env.REACT_APP_API}/scores`}),
     tagTypes:['Scores'],
     endpoints: (builder) => ({
         getScores: builder.query({
